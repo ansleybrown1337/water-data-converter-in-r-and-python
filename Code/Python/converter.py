8044f203d36e47524b52d7951abcfb995a681809
ansleybrown1337 <ansleybrown1337@gmail.com>
2023-10-05 17:11:20 -0600

    finished v1 of old to new fxn

--- Code/Python/converter.py
+++ Code/Python/converter.py
@@ -9,71 +9,175 @@ Description:
 This script provides functionalities to convert between new and old water 
 quality data formats. It's designed as both a utility tool and an educational 
 resource to showcase data conversion methodologies in Python.
+
+TODO:
+- Old to new
+    -  see why resulting pre/post conversion cols lists are different lengths
+- New to old
+    - Add functionality to convert new to old data format
+
+Sample usage (assuming new_data_format_sample is the new data format sample):
+>>> old_data = pd.read_csv("path_to_old_data.csv", header=1)
+>>> old_to_new_df = convert_old_to_new_final_fixed(
+    old_data, format_dictionary_updated)
 '''
 
 # Import packages
 import pandas as pd
 
 # Import data
-df_old = pd.read_csv('data/old_data_format.csv')
-df_new = pd.read_csv('data/new_data_format.csv')
+old_data = pd.read_csv('Example Old Data/old_data_format.csv', header=1)
+new_data = pd.read_csv('Example Old Data/new_data_format.csv')
 
-# Create dictonary to map equivalent columns and analyte names between formats
-format_dictionary = {
+old_cols = ['Location', 'Date', 'Irr/Storm', 'Station', 'Dup', 'event', 'ID', 'op',
+       'no2', 'no3', 'tkn', 'tp', 'selenium (mg/L)', 'TSS', 'E COLI', 'fecal',
+       'tss', 'Flow', 'OP', 'NO3', 'NH4', 'TKN', 'TP', 'E COLI.1', 'fecal.1',
+       'notes']
+
+new_cols = ['sample.id', 'lab.id.x', 'method', 'cas.number', 'analyte', 'result',
+       'units', 'dilution', 'result.reported.to', 'mdl', 'rl', 'report.basis',
+       'percent.moisture', 'percent.solid', 'non.detect', 'duplicate',
+       'location.name', 'treatment.name', 'method.name', 'event.type',
+       'event.count', 'lab.id.y', 'matrix', 'collected', 'received', 'hold',
+       'flag']
+
+# Data cleaning as necessary
+def clean_old_data(old_df):
+    """
+    Cleans the old data format to prepare for conversion.
+    inputs:
+        old_df: pandas dataframe of old data format
+    """
+    # Strip trailing spaces from column names in old_data
+    old_df.columns = old_df.columns.str.strip()
+    # identify duplicates and fill into Dup column
+        # sample ID's containing "-D" are duplicates
+        # note that contains() auto assigns True/False values here
+    old_df['Dup'] = old_df['ID'].str.contains('-D')
+    # identify nan values and fill over total dataframe
+        # if any cell contains NA, U, ND, nan, NAN, N/A, or is blank, replace with nan
+    old_df = old_df.replace(
+        ['NA', 'U', 'ND', 'nan', 'NAN', 'N/A', ''], pd.NA)
+    # correct MT, ST, CT, and IN to be MT1, ST1, CT1, and INF, respectively
+    old_df['Station'] = old_df['Station'].replace(
+        {'MT': 'MT1', 'ST': 'ST1', 'CT': 'CT1', 'IN': 'INF'})
+    # correct E COLI to be e coli
+    old_df['op'] = old_df['op'].replace({'E COLI': 'e coli'})
+
+    return old_df
+
+# Create dictionary to map equivalent columns and analyte names between formats
+format_dictionary_updated = {
     'op': 'Phosphorus, Total Orthophosphate (as P)',
     'no2': 'Nitrogen, Nitrite  (As N)',
     'no3': 'Nitrogen, Nitrate (As N)',
     'tkn': 'Nitrogen, Total Kjeldahl',
     'tp': 'Phosphorus, Total (As P)',
     'selenium (mg/L)': 'Selenium, Total',
-    'tss': 'TSS',
+    'TSS': ['TSS', 'Suspended Solids (Residue, Non-Filterable)'],
     'Location': 'location.name',
     'Date': 'date',
     'Irr/Storm': 'event.count',
     'Station': 'treatment.name',
     'Dup': 'duplicate',
     'ID': 'sample.id'
-    # the remaining columns have no overlap between formats
 }
 
-# Define functions
-def convert_old_to_new(old_df, format_dict, output_csv = False):
-    '''
+def convert_old_to_new_final_fixed(old_df, format_dict, output_csv=False):
+    """
     Converts old data format to new data format.
     inputs:
         old_df: pandas dataframe of old data format
         format_dict: dictionary of new/old column names and analyte names
-    '''
-    # Pivot the data based on the 'analyte' column using the new format_dict
-    pivoted_data = (
-        new_data.pivot_table(
-            index=[format_dict['ID'], format_dict['Location'], 
-                   format_dict['Date'], 'event.type', format_dict['Station'],
-                   'event'],
-            columns='analyte',
-            values='result',
-            aggfunc='first'
-        )
-        .reset_index()
-    )
+    """
+    # Clean the old data format
+    old_df = clean_old_data(old_df)
 
+    # Add method dataframe
+    method_df = pd.DataFrame({
+    'method': ['M4500 NH3 D - TKN_W 4500NH3 D', 'E300 - 300_W', 'E300 - 300_W', 'SM4500P E - P-Ortho M4500P E',
+               'SM4500P E - P_TW M4500P E', 'E160.1 - TDS_W_160.1', 'E120.1 - COND_W', 'SW9040C - pH_W_9040C',
+               'SW9040C - pH_W_9040C', 'E160.2 - TSS_W_160.2', '200.8'],
+    'cas.number': ['7727-37-9TKN', '14797-55-8', '14797-65-0', '7723-14-0', '7723-14-0', 'TDS', 'COND', 'PH', 'TEMP', 'TSS', 'SE'],
+    'analyte': ['Nitrogen, Total Kjeldahl', 'Nitrogen, Nitrate (As N)', 'Nitrogen, Nitrite  (As N)', 
+                'Phosphorus, Total Orthophosphate (as P)', 'Phosphorus, Total (As P)', 'Total Dissolved Solids (Residue, Filterable)',
+                'EC', 'pH', 'Temp Deg C @pH', 'TSS', 'Selenium, Total'],
+    'mdl': [0.1, 0.15, 0.15, 0.01, 0.02, 5, 5, 0.1, 0, 2.5, 0.2],
+    'rl': [0.5, 0.5, 0.5, 0.05, 0.05, 10, 5, 0.1, 0, 2.5, 0.2] #note RL was not reported by ALS for selenium, just MDL
+    })
+    
+    # Extract analytes from the format dictionary
+    analyte_cols = [key for key in format_dict.keys() if key not in ['Location', 'Date', 'Irr/Storm', 'Station', 'Dup', 'ID']]
+    
+    # Melt the old dataframe to convert analyte columns to rows
+    melted_data = old_df.melt(id_vars=[key for key in format_dict.keys() if key not in analyte_cols],
+                              value_vars=analyte_cols,
+                              var_name='analyte',
+                              value_name='result')
+    
+    # Map the old analyte names to the new ones using format_dict
+    # If the mapping is a list, default to the first value in the list
+    melted_data['analyte'] = melted_data['analyte'].apply(lambda x: format_dict[x][0] if isinstance(format_dict[x], list) else format_dict[x])
+    
+    ## Add default values or calculate new values for non-intersecting columns
+
+    # Add 'units' column with appropriate default values
+    melted_data['units'] = "mg/L"
+    melted_data.loc[melted_data['analyte'] == 'Selenium, Total', 'units'] = "ug/L"
+    melted_data.loc[melted_data['analyte'] == 'EC', 'units'] = "umhos/cm"
+    melted_data.loc[melted_data['analyte'] == 'pH', 'units'] = "pH"
+    melted_data.loc[melted_data['analyte'] == 'Temp Deg C @pH', 'units'] = "Celcius"
+    # add warning for selenium to double check units
+    if 'Selenium, Total' in melted_data['analyte'].values:
+        print("Warning: Selenium detected. Please double-check the units for Selenium.")
+
+    # Identify 0 values in the result column and set 'non.detect' column to True
+    melted_data['non.detect'] = melted_data['result'] == 0
+
+    # Merge with method_df to add method and cas.number columns
+    merged_data = pd.merge(melted_data, method_df, on='analyte', how='left')
+    
     # Rename columns based on format_dict for consistency
-    pivoted_data.rename(
-        columns={value: key for key, value in format_dict.items()},
-        inplace=True)
+    merged_data.rename(columns=format_dict, inplace=True)
 
-    # Merging back to get non-analyte columns
-    merged_data = pd.merge(
-        new_data.drop_duplicates(subset=[format_dict['ID']]),
-        pivoted_data,
-        on=[format_dict['ID'], format_dict['Location'], format_dict['Date'], 
-            'event.type', format_dict['Station'], 'event'],
-        how='left'
-    )
+    # Ensure all columns from the new format are present
+    for col in new_cols:
+        if col not in merged_data.columns:
+            merged_data[col] = pd.NA
 
-    # Output the data as a csv if desired
-    if output_csv == False:
-        pd.DataFrame.to_csv(merged_data, 'Output/converted_old_to_new_data.csv')
-        
-    # Return the data for further processing if desired        
+    # Export to csv to Output folder if output_csv is True
+    if output_csv:
+        merged_data.to_csv('Output/old_to_new_data.csv', index=False)
+    
     return merged_data
+
+# Diagnostic code to check the conversion in Python only ######################
+
+def report_non_overlapping_columns(old_df, new_df, format_dict):
+    """
+    Reports the columns that do not overlap during the conversion process.
+    
+    inputs:
+        old_df: pandas dataframe of old data format
+        new_df: pandas dataframe of new data format
+        format_dict: dictionary of new/old column names and analyte names
+        
+    returns:
+        non_overlapping_old: columns in old_df that are not keys in format_dict
+        non_overlapping_new: columns in new_df that are not in the converted data
+
+    Example usage:
+    non_overlapping_old, non_overlapping_new = report_non_overlapping_columns(
+        old_data, new_data, format_dictionary_updated)
+    """
+    
+    # Convert the old data format using the main conversion function
+    converted_data = convert_old_to_new_final(old_df, format_dict)
+    
+    # Identify columns in old_df that are not keys in format_dict
+    non_overlapping_old = set(old_df.columns) - set(format_dict.keys())
+    
+    # Identify columns in new_df that are not in the converted data
+    non_overlapping_new = set(new_df.columns) - set(converted_data.columns)
+    
+    return non_overlapping_old, non_overlapping_new
